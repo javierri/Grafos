c4a2a45378b2157a391c11adcf55a6cf75cf412e
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-11-16 20:58:49 -0400

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -6,18 +6,36 @@ class Nodo:
 		self.info = valor
 		self.arcos = []
 		
-	def enlace (self, destino, peso = 1):
-		if (type(destino) == type(self)):
-			arco = Arco(destino, peso)
+	def enlace (self, ndestino, peso = 1, bdir = False):
+		if (type(ndestino) == type(self)):
+			arco = Arco(ndestino, peso)
 			self.arcos.append(arco)
+			if (bdir == True):
+				arco = Arco(self, peso)
+				ndestino.arcos.append(arco)
 			return True
-			
 		return False
 		
 	def muestra_enlaces (self):
 		for arco in self.arcos: 
 			print arco.nodo.info,
 			print arco.peso
+			
+	def existe_enlace(self, ndestino):
+		for arco in self.arcos:
+			if (arco.nodo == ndestino):
+				return arco
+		return False
+		
+	def eli_enlace (self, ndestino):
+		arco = self.existe_enlace(ndestino)
+		if (arco != False):
+			self.arcos.remove(arco)
+			return True
+		return False
+			
+	def __del__(self):
+		del self.arcos
 		
 class Arco:
 	def __init__ (self, ndestino, peso=0):
@@ -25,8 +43,9 @@ class Arco:
 		self.peso = peso
 
 class Grafo:
-	def __init__(self):
+	def __init__(self, dirigido = True):
 		self.__nodos = []
+		self.__dirigido = dirigido
 		
 	def buscaNodo (self, valor):
 		for nodo in self.__nodos:
@@ -34,17 +53,20 @@ class Grafo:
 				return nodo
 		return False
 	
-	def enlace(self, origen, destino, peso = 1):
+	def enlace(self, valOrigen, valDestino, peso = 1, bdir = False):
 		
-		norigen = self.buscaNodo(origen)
+		norigen = self.buscaNodo(valOrigen)
 		if (not(norigen)):
 			return False
 			
-		ndestino = self.buscaNodo(destino)
+		ndestino = self.buscaNodo(valDestino)
 		if (not(ndestino)):
 			return False
 		
-		norigen.enlace(ndestino,peso)
+		if (self.__dirigido == False):
+			bdir = True
+			
+		norigen.enlace(ndestino, peso, bdir)
 		return True
 		
 	def ins_nodo (self, valor):
@@ -52,9 +74,44 @@ class Grafo:
 			nodo = Nodo(valor)
 			self.__nodos.append(nodo)
 			return nodo
-			
 		return False
-				
+		
+	def eli_nodo(self, valor):
+		nodoE = self.buscaNodo(valor)
+		if (nodoE == False):
+			return False
+			
+		for nodo in self.__nodos:
+			nodo.eli_enlace(nodoE)
+		
+		self.__nodos.remove(nodoE)
+		return True
+	
+	def existen_islas(self):
+		for nodo in self.__nodos:
+			if (len(nodo.arcos) == 0):
+				esIsla = True
+				for norigen in self.__nodos:
+					if (norigen.existe_enlace(nodo) != False):
+						esIsla = False
+						break
+						
+				if (esIsla == True):
+					return True
+		return False
+		
+	def __str__(self):
+		grafo  = ""
+		for nodo in self.__nodos:
+			grafo = grafo + nodo.info
+			arcos = ""
+			for arco in nodo.arcos:
+				if (arcos != ""):
+					arcos = arcos + ", "
+				arcos = arcos + arco.nodo.info + ":" + str(arco.peso)
+			grafo = grafo + "(" + arcos + ") "
+		return grafo	
+					
 # Principal
 
 g = Grafo()
@@ -68,26 +125,33 @@ nodo1.enlace(nodo3,1)
 nodo2.enlace(nodo4,1)
 
 print " GRAFO "
+print g 
+
 print "Enlaces de A"
 nodo1.muestra_enlaces()
 print "Enlaces de B"
 nodo2.muestra_enlaces()
 
-#######
+####### xxx #######
 
-g2 = Grafo()
+g2 = Grafo(False)
 				
 g2.ins_nodo("A")
 g2.ins_nodo("B")
 g2.ins_nodo("C")
 g2.ins_nodo("D")
+g2.ins_nodo("E")
 
 g2.enlace("A","B",2)
 g2.enlace("A","C",1)
 g2.enlace("B","D",1)
-		
+g2.enlace("D","E",1)
+
 print		
-print " GRAFO 2"		
+print " GRAFO 2"
+
+print g2
+		
 print "Enlaces de A"
 nodoA = g2.buscaNodo("A")
 nodoA.muestra_enlaces()
@@ -96,5 +160,10 @@ print "Enlaces de B"
 nodoB = g2.buscaNodo("B")
 nodoB.muestra_enlaces()
 		
-	
-	
+print "Islas..", g2.existen_islas()
+
+g2.eli_nodo("D")	
+
+print "Islas..", g2.existen_islas()
+
+print g2
